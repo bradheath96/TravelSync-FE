771682fa81a59820db12d2f45733028cad27bc01
bradheath96 <bradheath96@gmail.com>
2024-08-19 09:44:52 +0100

    added more mock data and some custom basic css to the mapbox

--- src/components/MapBox.jsx
+++ src/components/MapBox.jsx
@@ -7,37 +7,46 @@ import { data } from "../utils/mockData";
 mapboxgl.accessToken = mapBoxAccessCode;
 
 const MapBox = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  const [lng, setLng] = useState(-2.2426);
-  const [lat, setLat] = useState(53.4808);
-  const [zoom, setZoom] = useState(12);
+	const mapContainer = useRef(null);
+	const map = useRef(null);
+	const [lng, setLng] = useState(-2.2426);
+	const [lat, setLat] = useState(53.4808);
+	const [zoom, setZoom] = useState(12);
 
-  useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: "mapbox://styles/mapbox/streets-v12",
-      center: [lng, lat],
-      zoom: zoom,
-    });
+	useEffect(() => {
+		if (map.current) return;
+		map.current = new mapboxgl.Map({
+			container: mapContainer.current,
+			style: "mapbox://styles/mapbox/streets-v12",
+			center: [lng, lat],
+			zoom: zoom,
+		});
 
-    data.forEach((location) => {
-      new mapboxgl.Marker()
-        .setLngLat(location.coordinates)
-        .setPopup(new mapboxgl.Popup().setHTML(`<h4>${location.name}</h4>`))
-        .addTo(map.current);
-    });
-    console.log(map);
-  }, [data]);
+		data.forEach((location) => {
+			console.log(location);
+			new mapboxgl.Marker()
+				.setLngLat([location.lng, location.lat])
+				.setPopup(
+					new mapboxgl.Popup().setHTML(
+						`<div class="popup-container">
+						<h4 class="popup-title">${location.name}</h4>
+						<p>Rating: ${location.rating}</p>
+						<button class="popup-button">Learn More</button>
+						</div>`
+					)
+				)
+				.addTo(map.current);
+		});
+		console.log(map);
+	}, [data]);
 
-  return (
-    <div
-      ref={mapContainer}
-      style={{ width: "100%", height: "500px" }}
-      className="map-container"
-    />
-  );
+	return (
+		<div
+			ref={mapContainer}
+			style={{ width: "100%", height: "500px" }}
+			className="map-container"
+		/>
+	);
 };
 
 export default MapBox;
