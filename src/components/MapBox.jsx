05431fdf71214bd0c0853980caa5c37846b83c29
bradheath96 <bradheath96@gmail.com>
2024-08-19 15:16:03 +0100

    added search bar

--- src/components/MapBox.jsx
+++ src/components/MapBox.jsx
@@ -2,8 +2,9 @@ import React, { useRef, useEffect, useState } from "react";
 import mapboxgl from "mapbox-gl";
 import mapBoxAccessCode from "../utils/map_box_access_key";
 import "../CSS/map_box.css";
-import { data } from "../utils/mockData";
+import { data, dataMockTwo } from "../utils/mockData";
 import { Link, useNavigate } from "react-router-dom";
+import SearchBar from "./SearchBar";
 
 mapboxgl.accessToken = mapBoxAccessCode;
 
@@ -13,6 +14,8 @@ const MapBox = () => {
 	const [lng, setLng] = useState(-2.2426);
 	const [lat, setLat] = useState(53.4808);
 	const [zoom, setZoom] = useState(12);
+	const [search, setSearch] = useState("");
+	const [filteredLocations, setFilteredLocations] = useState([]);
 
 	const navigate = useNavigate();
 
@@ -24,15 +27,19 @@ const MapBox = () => {
 			center: [lng, lat],
 			zoom: zoom,
 		});
+	});
 
-		data.forEach((location) => {
+	useEffect(() => {
+		handleSearch();
+		filteredLocations.forEach((location) => {
 			const popupContent = document.createElement("div");
-			popupContent.innerHTML = `<div class="popup-container">
+			popupContent.innerHTML = `
+						<div class="popup-container">
 						<h4 class="popup-title">${location.name}</h4>
 						<p>Rating: ${location.rating}</p>
-						<button class="popup-button">
-            View Details</button>
-						</div>`;
+						<button class="popup-button">View Details</button>
+						</div>
+					`;
 			popupContent
 				.querySelector(".popup-button")
 				.addEventListener("click", () => {
@@ -41,19 +48,52 @@ const MapBox = () => {
 					});
 				});
 			new mapboxgl.Marker()
-				.setLngLat([location.lng, location.lat])
+				.setLngLat([location["co-ords"].lng, location["co-ords"].lat])
 				.setPopup(new mapboxgl.Popup().setDOMContent(popupContent))
 				.addTo(map.current);
 		});
-		console.log(map);
-	}, [data, navigate]);
+		console.log(search, "<<< search")
+	}, [search]);
+
+	const handleSearch = () => {
+		const filtered = dataMockTwo.filter((location) => {
+			return location.name.toLowerCase().includes(search.toLowerCase());
+		});
+		setFilteredLocations(filtered);
+	};
+	console.log(filteredLocations, "<<< filtered")
+
+
+	// 	dataMockTwo.forEach((location) => {
+	// const popupContent = document.createElement("div");
+	// popupContent.innerHTML = `
+	// 					<div class="popup-container">
+	// 					<h4 class="popup-title">${location.name}</h4>
+	// 					<p>Rating: ${location.rating}</p>
+	// 					<button class="popup-button">View Details</button>
+	// 					</div>
+	// 				`;
+	// popupContent.querySelector(".popup-button").addEventListener("click", () => {
+	// 	navigate(`/location/${location.id}`, {
+	// 		state: { location: location },
+	// 	});
+	// });
+	// 		new mapboxgl.Marker()
+	// 			.setLngLat([location["co-ords"].lng, location["co-ords"].lat])
+	// 			.setPopup(new mapboxgl.Popup().setDOMContent(popupContent))
+	// 			.addTo(map.current);
+	// 	});
+	// }, [dataMockTwo, navigate]);
 
 	return (
-		<div
-			ref={mapContainer}
-			style={{ width: "100%", height: "500px" }}
-			className="map-container"
-		/>
+		<div>
+			<SearchBar setSearch={setSearch} />
+			<div
+				ref={mapContainer}
+				style={{ width: "100%", height: "500px" }}
+				className="map-container"
+			/>
+		</div>
 	);
 };
 
