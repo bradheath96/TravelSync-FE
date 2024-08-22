b6e2e6aa7e0e6a70ae5c16629a1e95751ae736b1
bradheath96 <bradheath96@gmail.com>
2024-08-22 10:43:42 +0100

    updated the rating undefined bug and added more mock data

--- src/utils/mockData.js
+++ src/utils/mockData.js
@@ -1,67 +1,5 @@
-export const data = [
-	{
-		id: 1,
-		name: "Etihad Stadium",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit eleifend tortor non sagittis. Etiam leo sem, vulputate in elit non, dictum finibus nulla. Vestibulum vulputate finibus pretium. Nam tempor dui dui, vel fermentum risus consequat tincidunt",
-		openingTimes: "9am - 5pm",
-		lng: -2.2004,
-		lat: 53.4831,
-		rating: 4.4,
-	},
-	{
-		id: 2,
-		name: "Old Trafford",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit eleifend tortor non sagittis. Etiam leo sem, vulputate in elit non, dictum finibus nulla. Vestibulum vulputate finibus pretium. Nam tempor dui dui, vel fermentum risus consequat tincidunt",
-		openingTimes: "9am - 5pm",
-		lng: -2.2913,
-		lat: 53.4631,
-		rating: 4.8,
-	},
-	{
-		id: 3,
-		name: "Manchester Art Gallery",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit eleifend tortor non sagittis. Etiam leo sem, vulputate in elit non, dictum finibus nulla. Vestibulum vulputate finibus pretium. Nam tempor dui dui, vel fermentum risus consequat tincidunt",
-		openingTimes: "9am - 5pm",
-		lng: -2.2413,
-		lat: 53.4796,
-		rating: 4.6,
-	},
-	{
-		id: 4,
-		name: "John Rylands Library",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit eleifend tortor non sagittis. Etiam leo sem, vulputate in elit non, dictum finibus nulla. Vestibulum vulputate finibus pretium. Nam tempor dui dui, vel fermentum risus consequat tincidunt",
-		openingTimes: "9am - 5pm",
-		lng: -2.2479,
-		lat: 53.4795,
-		rating: 4.9,
-	},
-	{
-		id: 5,
-		name: "Heaton Park",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit eleifend tortor non sagittis. Etiam leo sem, vulputate in elit non, dictum finibus nulla. Vestibulum vulputate finibus pretium. Nam tempor dui dui, vel fermentum risus consequat tincidunt",
-		openingTimes: "9am - 5pm",
-		lng: -2.2503,
-		lat: 53.5459,
-		rating: 4.7,
-	},
-	{
-		id: 6,
-		name: "People's History Museum",
-		description:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. In suscipit eleifend tortor non sagittis. Etiam leo sem, vulputate in elit non, dictum finibus nulla. Vestibulum vulputate finibus pretium. Nam tempor dui dui, vel fermentum risus consequat tincidunt",
-		openingTimes: "9am - 5pm",
-		lng: -2.2512,
-		lat: 53.482,
-		rating: 4.5,
-	},
-];
 
-export const dataMockTwo = [
+const dataMock = [
 	{
 		"place-id": 1,
 		name: "Etihad Stadium",
@@ -203,3 +141,17 @@ export const dataMockTwo = [
 		"user-ratings-total": 75,
 	},
 ];
+
+
+const dataMockForTables = {
+	tasks: {
+		"task-1": { id: 1, name: "Etihad Stadium" },
+		"task-2": { id: 2, name: "Old Trafford" },
+		"task-3": { id: 3, name: "Manchester Art Gallery" },
+		"task-4": { id: 4, name: "The John Rylands Library" },
+		"task-5": { id: 5, name: "Science and Industry Museum" },
+		"task-6": { id: 6, name: "The Lowry" },
+		"task-7": { id: 7, name: "National Football Museum" },
+		"task-8": { id: 8, name: "Manchester Cathedral" },
+	},
+};
