a6e4132c76772f2ff794d696750a54098477a9c3
dudleyspence <dudleyspence5@gmail.com>
2024-08-29 14:15:33 +0200

    adds fileStorage styling

--- src/components/FilesPage.jsx
+++ src/components/FilesPage.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import MyDropzone from "./Dropzone";
+import deleteBin from "../assets/deleteBin.png";
+import documentIcon from "../assets/document.png";
 
 export default function FilesPage() {
   return (
@@ -7,7 +9,25 @@ export default function FilesPage() {
       <h1>Holiday Files</h1>
       <MyDropzone />
       <h3>Your Group Files:</h3>
-      <div className="filesListContainer"></div>
+      <div className="filesListContainer">
+        <ul>
+          <li className="savedFile">
+            <img src={documentIcon} alt="pdf" className="documentIcon" />
+            <p>FlightTickets.pdf</p>
+            <img src={deleteBin} className="binImg" alt="delete button" />
+          </li>
+          <li className="savedFile">
+            <img src={documentIcon} alt="pdf" className="documentIcon" />
+            <p>AirbnbBooking.pdf</p>
+            <img src={deleteBin} className="binImg" alt="delete button" />
+          </li>
+          <li className="savedFile">
+            <img src={documentIcon} alt="pdf" className="documentIcon" />
+            <p>VISA-Granted.pdf</p>
+            <img src={deleteBin} className="binImg" alt="delete button" />
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
