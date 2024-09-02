c8409fa7fa8d148d08435ce770b615a0426f1361
dudleyspence <dudleyspence5@gmail.com>
2024-09-02 16:15:34 +0200

    update

--- src/components/Auth/SignUp.jsx
+++ src/components/Auth/SignUp.jsx
@@ -1,86 +1,101 @@
 import { useState } from "react";
 import { Navigate, Link, useNavigate } from "react-router-dom";
-import { useAuth } from "../Context/AuthContext/index";
-import { doCreateUserWithEmailAndPassword } from "../../firebase/auth";
+import { useAuth } from "../Context/AuthContext";
+import { doCreateUserWithEmailAndPassword } from "../../firebase/auth"; // Import only the functions you need
+import logo from "../../assets/TravelSync.png";
+import { saveNewUser } from "../../axios"; // Assume this is the function to save user to your database
 
 export default function SignUp() {
   const navigate = useNavigate();
-
+  const { userLoggedIn } = useAuth();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [confirmPassword, setconfirmPassword] = useState("");
+  const [name, setName] = useState(""); // This will be saved to your own database
+  const [confirmPassword, setConfirmPassword] = useState("");
   const [isRegistering, setIsRegistering] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const { userLoggedIn } = useAuth();
+  function onSubmit(event) {
+    event.preventDefault();
 
-  const onSubmit = async (e) => {
-    e.preventDefault();
     if (!isRegistering) {
+      if (password !== confirmPassword) {
+        setErrorMessage("Passwords do not match");
+        return;
+      }
+
       setIsRegistering(true);
-      await doCreateUserWithEmailAndPassword(email, password);
+
+      doCreateUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          const user = userCredential.user;
+
+          return saveNewUser(user.uid, name, email);
+        })
+        .then(() => {
+          navigate("/");
+        })
+        .catch((error) => {
+          console.error("Error during sign-up process:", error);
+          setErrorMessage(error.message);
+        })
+        .finally(() => {
+          setIsRegistering(false);
+        });
     }
-  };
+  }
 
   return (
-    <>
+    <div>
       {userLoggedIn && <Navigate to={"/"} replace={true} />}
-
-      <main className="w-full h-screen flex self-center place-content-center place-items-center">
-        <div className="w-96 text-gray-600 space-y-5 p-4 shadow-xl border rounded-xl">
-          <div className="text-center mb-6">
-            <div className="mt-2">
-              <h3 className="text-gray-800 text-xl font-semibold sm:text-2xl">
-                Create a New Account
-              </h3>
-            </div>
-          </div>
-          <form onSubmit={onSubmit} className="space-y-4">
+      <div className="login">
+        <img src={logo} alt="our Logo" className="loginLogo" />
+        <div className="loginFormContainer">
+          <h3>Create a New Account</h3>
+          <form onSubmit={onSubmit} className="sign-up-form">
             <div>
-              <label className="text-sm text-gray-600 font-bold">Email</label>
+              <label>Full Name</label>
+              <input
+                type="text"
+                required
+                value={name}
+                onChange={(event) => setName(event.target.value)}
+                className="form-input"
+              />
+            </div>
+            <div>
+              <label>Email</label>
               <input
                 type="email"
                 autoComplete="email"
                 required
                 value={email}
-                onChange={(e) => {
-                  setEmail(e.target.value);
-                }}
-                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:indigo-600 shadow-sm rounded-lg transition duration-300"
+                onChange={(e) => setEmail(e.target.value)}
+                className="form-input"
               />
             </div>
-
             <div>
-              <label className="text-sm text-gray-600 font-bold">
-                Password
-              </label>
+              <label>Password</label>
               <input
                 disabled={isRegistering}
                 type="password"
                 autoComplete="new-password"
                 required
                 value={password}
-                onChange={(e) => {
-                  setPassword(e.target.value);
-                }}
-                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                onChange={(e) => setPassword(e.target.value)}
+                className="form-input"
               />
             </div>
-
             <div>
-              <label className="text-sm text-gray-600 font-bold">
-                Confirm Password
-              </label>
+              <label>Confirm Password</label>
               <input
                 disabled={isRegistering}
                 type="password"
                 autoComplete="off"
                 required
                 value={confirmPassword}
-                onChange={(e) => {
-                  setconfirmPassword(e.target.value);
-                }}
-                className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg transition duration-300"
+                onChange={(e) => setConfirmPassword(e.target.value)}
+                className="form-input"
               />
             </div>
 
@@ -91,26 +106,19 @@ export default function SignUp() {
             <button
               type="submit"
               disabled={isRegistering}
-              className={`w-full px-4 py-2 text-white font-medium rounded-lg ${
-                isRegistering
-                  ? "bg-gray-300 cursor-not-allowed"
-                  : "bg-indigo-600 hover:bg-indigo-700 hover:shadow-xl transition duration-300"
-              }`}
+              className="styled-button sign-up-button"
             >
               {isRegistering ? "Signing Up..." : "Sign Up"}
             </button>
-            <div className="text-sm text-center">
-              Already have an account? {"   "}
-              <Link
-                to={"/login"}
-                className="text-center text-sm hover:underline font-bold"
-              >
-                Continue
-              </Link>
-            </div>
           </form>
+          <p>
+            Already have an account?{" "}
+            <Link to={"/login"} className="hover:underline font-bold">
+              Continue
+            </Link>
+          </p>
         </div>
-      </main>
-    </>
+      </div>
+    </div>
   );
 }
--- src/components/HomePage/Homepage.jsx
+++ src/components/HomePage/Homepage.jsx
@@ -2,7 +2,6 @@ import { useState, useContext, useEffect } from "react";
 import CreateItinerary from "./CreateItinerary";
 import JoinItinerary from "./JoinItinerary";
 import UserItinerariesList from "./UserItinerariesList";
-import { UserContext } from "../Context/UserContextProvider";
 import logo from "../../assets/TravelSync.png";
 import { useNavigate } from "react-router-dom";
 import { ItineraryContext } from "../Context/ItineraryContextProvider";
@@ -10,9 +9,10 @@ import { useAuth } from "../Context/AuthContext/index";
 import { doSignOut } from "../../firebase/auth";
 
 export default function Homepage() {
-  const { userLoggedIn } = useContext(UserContext);
+  const { userLoggedIn } = useAuth();
   const { setCurrentItinerary } = useContext(ItineraryContext);
   const { currentUser } = useAuth();
+  console.log(currentUser);
 
   const [itineraryUpdates, setItineraryUpdates] = useState(false);
 
@@ -35,14 +35,11 @@ export default function Homepage() {
           Sign Out
         </button>
         <img className="logo" src={logo} alt="logo" />
-        <h1>Hello {userLoggedIn.username}</h1>
+        <h1>Hello {currentUser.displayName}</h1>
         <div className="profileSection"></div>
         <div className="userItineraryControls">
           <CreateItinerary setItineraryUpdates={setItineraryUpdates} />
-          <JoinItinerary
-            userLoggedIn={userLoggedIn}
-            setItineraryUpdates={setItineraryUpdates}
-          />
+          <JoinItinerary setItineraryUpdates={setItineraryUpdates} />
         </div>
         <div className="userItineraryList">
           <h4>Itineraries:</h4>
